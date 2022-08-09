73ed69759627724ab0d6d1afd689415f3db76586
Rizwan Manzoor <m.rizwan.xyz@gmail.com>
2022-08-09 22:11:38 +0500

    MySQL Setup Finished

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     database: 'blog',
     user: 'root',
-    password: 'MynameRixwan0011'
+    password: '********'
 });
 
 module.exports = pool;
